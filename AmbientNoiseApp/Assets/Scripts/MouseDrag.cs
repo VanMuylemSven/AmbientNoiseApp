72b0c9b22da15442214fe79d3ffb356c7a542979
sven <sven0567@hotmail.com>
2018-11-08 11:27:59 +0100

    Added Mousedrag script for dragging audio objects

---
+++ AmbientNoiseApp/Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour {
+
+    public float _distance = 10;
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("Dragging from " + transform.position);
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _distance); //distance instead of pos.z?
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Debug.Log("To pos: " + objectPos); ///////////////////
+
+        this.transform.position = mousePos;
+    }
+}
