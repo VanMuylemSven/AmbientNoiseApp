47db6926c4930b662bc6f701278687b59a0153d5
sven <sven0567@hotmail.com>
2018-11-29 22:14:37 +0100

    Added Delete and ToggleSlider buttons

---
+++ AmbientNoiseApp/Assets/Scripts/ToggleObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleSlider()
+    {
+        if(this.gameObject.activeSelf)
+            this.gameObject.SetActive(false);
+        else
+            this.gameObject.SetActive(true);
+    }
+}
