2bb524e7d5e9cf6bcab9a4f33c32fb997fdbb599
Kenzo Warrens <kenzo.warrens@student.howest.be>
2018-12-01 21:25:03 +0100

    added instructions on boot that selfdestruct after 3 seconds

---
+++ AmbientNoiseApp/Assets/Scripts/OverlayTextManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayTextManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SelfDestructAfterTimer());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SelfDestructAfterTimer()    
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(this.gameObject);
+    }    
+
+}
