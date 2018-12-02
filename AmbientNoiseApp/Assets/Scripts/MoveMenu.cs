6009d65d66ce354421b8a2825a8955c7789e4ecd
Annelynn Pyck <annelynn.pyck@student.howest.be>
2018-12-02 16:46:55 +0100

    added button to move menu but button acts weird

---
+++ AmbientNoiseApp/Assets/Scripts/MoveMenu.cs
@@ -0,0 +1,26 @@
+﻿using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveObject()
+    {
+        TapToPlaceOnce tapToPlace = this.GetComponent<TapToPlaceOnce>();
+
+        //set needed properties 
+        tapToPlace.HasBeenPlaced = false;
+        tapToPlace.IsBeingPlaced = true;
+    }
+}
